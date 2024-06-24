71d215fb3337159f0f7801dcfc567d09de534746
Sara Ahmad Malik <sarahahmadmalik55@gmail.com>
2024-06-24 20:54:55 +0500

    counter-done

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -6,7 +6,6 @@ const Counter = () => {
 
   return (
     <div>
-      <h1>Count: {count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
